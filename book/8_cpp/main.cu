d6bf1d12b076f8e3e798cf730f5271d8c082b233
zhbcolin <zhanghzobo@163.com>
2023-07-20 18:27:40 +0800

    learn in 2023/7/20

---
+++ book/8_cpp/main.cu
@@ -0,0 +1,30 @@
+#include "common.cuh"
+#include "memory.cuh"
+#include "initialize.cuh"
+#include "neighbor.cuh"
+#include "integrate.cuh"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int nx = 5;
+    int Ne = 20000;
+    int Np = 20000;
+
+    int N = 4 * nx * nx * nx;
+    int Ns = 100;
+    int MN = 200;
+    real T_0 = 60.0;
+    real ax = 5.385;
+    real time_step = 5.0 / TIME_UNIT_CONVERSION;
+    Atom atom;
+    allocate_memory(N, MN, &atom);
+    for(int n = 0; n < N; ++n) { atom.m[n] = 40.0; }
+    initialize_position(nx, ax, &atom);
+    initialize_velocity(N, T_0, &atom);
+    find_neighbor(N, MN, &atom);
+    equilibration(Ne, N, MN, T_0, time_step, &atom);
+    production(Np, Ns, N, MN, T_0, time_step, &atom);
+    deallocate_memory(&atom);
+    return 0;
+}
---
+++ book/8_cpp/memory.cu
@@ -0,0 +1,4 @@
+#include "memory.cuh"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
