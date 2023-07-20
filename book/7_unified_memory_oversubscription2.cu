729895d8437e35724b8a983d8787df3dbe4d103d
zhbcolin <zhanghzobo@163.com>
2023-07-20 14:05:57 +0800

    learn in 2023/7/20

---
+++ book/7_unified_memory_oversubscription2.cu
@@ -0,0 +1,27 @@
+#include "../include/error.cuh"
+#include <stdio.h>
+#include <stdint.h>
+
+const int N = 30;
+
+__global__ void gpu_touch(uint64_t *x, const size_t size) {
+    const size_t i = blockDim.x * blockIdx.x + threadIdx.x;
+    if(i < size) {
+        x[i] = 0;
+    }
+}
+
+int main() {
+    for(int n = 1; n <= N; ++n) {
+        const size_t memory_size = size_t(n) * 1024 * 1024 * 1024;
+        const size_t data_size = memory_size / sizeof(uint64_t);
+        uint64_t *x;
+        CHECK(cudaMallocManaged(&x, memory_size));
+        gpu_touch<<<(data_size-1)/1024+1, 1024>>>(x, data_size);
+        CHECK(cudaGetLastError());
+        CHECK(cudaDeviceSynchronize());
+        CHECK(cudaFree(x));
+        printf("Allocated %d GB unified memory with GPU touch.\n", n);
+    }
+    return 0;
+}
